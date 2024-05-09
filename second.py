5bca20e484c42cb3eb4b6f66e7b12fc41c820ad2
cheanz <59786755+cheanz@users.noreply.github.com>
2024-05-09 22:08:14 +0800

    Add files via upload

---
+++ second.py
@@ -0,0 +1,23 @@
+class Solution:
+    def comment(self, input :str ) -> str:
+        stack={}
+        stack.setdefault('(',[])
+        stack.setdefault(')',[])
+        #double stack
+        for i in range(len(input)):
+            if input[i] == '(':
+                stack['('].append(i)
+                print(stack)
+            elif input[i] == ')':
+                if stack['(']:
+                    stack['('].pop()
+                else:
+                    stack[')'].append(i)
+        commentstring=[' ']*len(input)
+        for m in stack['(']:
+            commentstring[m]='x'
+        for n in stack[')']:
+            commentstring[n]='?'
+        print(input)
+        print(''.join(commentstring))
+Solution.comment(Solution,"bge)))))))))")
---
+++ sub.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, t: str, s: str) -> int:
+        '''
+        t="abc"
+        s="abcbc"
+        '''
+        maxlengthofsub=len(t)
+        reallength=len(s)
+        for i in s:
+            if i not in t:
+                return -1
+            
+        m=0
+        sumofsubstrings=0
+        startindex=0
+
+        while startindex < reallength:
+            n=0
+            while m < maxlengthofsub and n < maxlengthofsub :
+                if startindex+m < reallength:
+                    if  s[startindex+m] == t[n]:
+                        m += 1
+                        print(m)
+                        print(n)
+                    else:
+                        n += 1
+            sumofsubstrings += 1
+            startindex = startindex + m + 1
+        return sumofsubstrings
+
+print(Solution.lengthOfLongestSubstring(Solution,'xyz','z'))

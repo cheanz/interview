29cee80811a47e64aa9d7eaa32a48fe45422c939
cheanz <59786755+cheanz@users.noreply.github.com>
2024-05-10 07:50:57 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -11,21 +11,24 @@ class Solution:
             if i not in t:
                 return -1
             
-        m=0
+        
         sumofsubstrings=0
         startindex=0
 
         while startindex < reallength:
             n=0
+            m=0
             while m < maxlengthofsub and n < maxlengthofsub :
                     if startindex+m >= reallength:
                         break
                     if  s[startindex+m] == t[n]:
-                        m += 1
-                    else:
-                        n += 1
+                        m += 1                    
+                    n += 1
             sumofsubstrings += 1
-            startindex = startindex + m - 1
+            startindex = startindex + m
+            # print('m:')
+            # print(m)
+            # print(startindex)
         return sumofsubstrings
 
-print(Solution.lengthOfLongestSubstring(Solution,'xyz','z'))
+print(Solution.lengthOfLongestSubstring(Solution,'abc','a'))
